8086c5a708dd8d0ea9df205372bc9c427c5cdf43
Aleksey Astafiev <aastafiev@gmail.com>
2023-11-21 22:50:32 +0300

    code refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,40 @@
-use crate::VERSION;
 use std::{io, io::prelude::*, process, collections::HashMap, env};
 use urlencoding::decode;
 use glib::markup_escape_text;
+use clap::{Command, Args, FromArgMatches, ArgMatches};
+
+const VERSION: &str= env!("CARGO_PKG_VERSION");
+
+#[derive(Debug, Args)]
+pub struct RofiArgs {
+    /// Set display
+    #[arg(short, long, default_value = ":0", env)]
+    display: String,
+
+    /// Set rofi prompt
+    #[arg(short, long, env = "PINENTRY_USER_DATA")]
+    prompt: Option<String>,
+}
+
+
+pub fn cmd() -> Command {
+    let cli = clap::command!()
+        .after_help("INSTALL:\n  \
+  1. Copy `pinentry-rofi` to your `~/.local/bin`.
+  2. `chmod +x ~/.local/bin/pinentry-rofi`.
+  3. Set `pinentry-program` in `~/.gnupg/gpg-agent.conf`. For example:
+     `pinentry-program <HOME>/.local/bin/pinentry-rofi`
+  4. Restart gpg-agent `gpgconf --kill gpg-agent`")
+        .help_template("\
+{before-help}{name} {version}
+{author-with-newline}
+{usage-heading} {usage}
+{after-help}
+
+{all-args}
+");
+    RofiArgs::augment_args(cli)
+}
 
 
 fn assuan_send(mesg: String) -> io::Result<()> {
@@ -54,9 +87,9 @@ pub fn handle_command(action: &str, arg: &str, rofi_args: &mut HashMap<String, O
         ("GETINFO", "pid") => { assuan_send(format!("D {}", process::id()))? },
         ("GETINFO", "ttyinfo") => {
             assuan_send(format!("D {0} {1} {2}",
-                                      env::var("GPG_TTY").expect("GPG_TTY environment variable not set"),
-                                      env::var("GPG_TERM").unwrap_or("".to_string()),
-                                      env::var("DISPLAY").expect("DISPLAY environment variable not set")))?
+                                    env::var("GPG_TTY").expect("GPG_TTY environment variable not set"),
+                                    env::var("GPG_TERM").unwrap_or("".to_string()),
+                                    env::var("DISPLAY").expect("DISPLAY environment variable not set")))?
         },
         ("GETINFO", "flavor") => { assuan_send("D keyring".to_string())? },
         ("GETINFO", "version") => { assuan_send(format!("D {}", VERSION))? },
@@ -94,13 +127,32 @@ pub fn handle_command(action: &str, arg: &str, rofi_args: &mut HashMap<String, O
 }
 
 
-pub fn pinentry(rofi_args: &mut HashMap<String, Option<String>>) -> io::Result<()> {
+pub fn pinentry(args_matches: &ArgMatches) -> io::Result<()> {
+    let args = RofiArgs::from_arg_matches(args_matches)
+        .map_err(|err| err.exit())
+        .unwrap();
+
+
+    let mut rofi_args = HashMap::from([
+        (String::from("-dmenu"), None),
+        (String::from("-display"), Some(args.display.to_string())),
+        (String::from("-input"), Some(String::from("/dev/null"))),
+        (String::from("-password"), None),
+        (String::from("-disable-history"), None),
+        (String::from("-l"), Some(String::from("0"))),
+    ]);
+
+    match args.prompt {
+        Some(prompt) => { rofi_args.insert(String::from("-prompt"), Some(prompt)); },
+        None => {},
+    }
+
     assuan_send("OK Please go ahead".to_string())?;
 
     for line in io::stdin().lock().lines() {
         let row = line?;
         let (action, arg) = row.split_once(' ').unwrap_or_else(|| (&row, ""));
-        handle_command(action, arg, rofi_args)?;
+        handle_command(action, arg, &mut rofi_args)?;
     }
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,9 @@
-mod pinentry;
+use std::io;
 
-use pinentry::pinentry;
-
-use clap::{Command, Args, FromArgMatches};
-// use clap::Parser;
-use std::{io, collections::HashMap};
-// use std::io;
-
-
-const VERSION: &str= env!("CARGO_PKG_VERSION");
-
-
-#[derive(Debug, Args)]
-struct RofiArgs {
-    /// Set display
-    #[arg(short, long, default_value = ":0", env)]
-    display: String,
-
-    /// Set rofi prompt
-    #[arg(short, long, env = "PINENTRY_USER_DATA")]
-    prompt: Option<String>,
-}
-
-
-fn cmd() -> Command {
-    let cli = clap::command!()
-        .after_help("INSTALL:\n  \
-  1. Copy `pinentry-rofi` to your `~/.local/bin`.
-  2. `chmod +x ~/.local/bin/pinentry-rofi`.
-  3. Set `pinentry-program` in `~/.gnupg/gpg-agent.conf`. For example:
-     `pinentry-program <HOME>/.local/bin/pinentry-rofi`
-  4. Restart gpg-agent `gpgconf --kill gpg-agent`")
-        .help_template("\
-{before-help}{name} {version}
-{author-with-newline}
-{usage-heading} {usage}
-{after-help}
-
-{all-args}
-");
-    RofiArgs::augment_args(cli)
-}
-
+use pinentry_rofi::{cmd, pinentry};
 
 fn main() -> io::Result<()> {
     let matches = cmd().get_matches();
-    let args = RofiArgs::from_arg_matches(&matches)
-        .map_err(|err| err.exit())
-        .unwrap();
-
-    let mut rofi_args = HashMap::from([
-        (String::from("-dmenu"), None),
-        (String::from("-display"), Some(args.display.to_string())),
-        (String::from("-input"), Some(String::from("/dev/null"))),
-        (String::from("-password"), None),
-        (String::from("-disable-history"), None),
-        (String::from("-l"), Some(String::from("0"))),
-    ]);
-
-    match args.prompt {
-        Some(prompt) => { rofi_args.insert(String::from("-prompt"), Some(prompt)); },
-        None => {},
-    }
-
-    pinentry(&mut rofi_args)?;
+    pinentry(&matches)?;
     Ok(())
 }
